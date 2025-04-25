5194b663d74a81f47800e266aa0bd2d22cc884f3
Harshita Singh <harshita.22bce8545@vitapstudent.ac.in>
2025-04-26 00:20:40 +0530

    changed files

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,16 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // <<< ADD THIS! very important for Render deployment
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
+    react({
+      jsxImportSource: "react", // <<< ADD THIS!
+    }),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
